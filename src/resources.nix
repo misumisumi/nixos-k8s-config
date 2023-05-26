68d456fb1089cad05c4d2d035f7b42c92c771bcd
Sumi-Sumi <dragon511southern@gmail.com>
2023-05-26 10:10:53 +0900

    update(src): provider-lxdに合うように変更

--- src/resources.nix
+++ src/resources.nix
@@ -1,5 +1,6 @@
 {lib, ...}: let
-  payload = builtins.fromJSON (builtins.readFile ../show.json);
+  pwd = builtins.toPath (builtins.getEnv "PWD");
+  payload = builtins.fromJSON (builtins.readFile "${pwd}/show.json");
   resourcesInModule = type: module: builtins.filter (r: r.type == type) (module.resources or []) ++ lib.flatten (map (resourcesInModule type) (module.child_modules or []));
   resourcesByType = type: resourcesInModule type payload.values.root_module;
 in rec {
--- src/utils.nix
+++ src/utils.nix
@@ -1,5 +1,6 @@
 {
-  nodeIP = r: let
-    interface = builtins.head r.values.network_interface;
-  in (builtins.head interface.addresses);
+  # nodeIP = r: let
+  #   interface = builtins.head r.values.network_interface;
+  # in (builtins.head interface.addresses);
+  nodeIP = r: r.values.ip_address;
 }
