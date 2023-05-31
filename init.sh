642f529028bb3b7d846a973dc5d4ff1c03300888
Sumi-Sumi <dragon511southern@gmail.com>
2023-06-01 00:58:04 +0900

    remove(*.sh): シェルスクリプトを.nixに統合

--- init.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-[[ ! -d ./.terraform ]] && terraform init
-[[ $(terraform workspace list | grep product) == "" ]] && terraform workspace new product
-[[ $(terraform workspace list | grep develop) == "" ]] && terraform workspace new develop
-
-echo "initialization complete"
