45d4588a79e97a351ff33e767d793c7de602d2af
Sumi-Sumi <dragon511southern@gmail.com>
2023-05-31 21:21:49 +0900

    fix(resources.nix): can use flake without show.json

--- src/resources.nix
+++ src/resources.nix
@@ -1,8 +1,12 @@
 {lib, ...}: let
   pwd = builtins.getEnv "PWD";
-  payload = builtins.fromJSON (builtins.readFile "${pwd}/show.json");
+  json = "${builtins.getEnv "PWD"}/show.json";
+  payload =
+    if builtins.pathExists json
+    then builtins.fromJSON (builtins.readFile json)
+    else {};
   resourcesInModule = type: module: builtins.filter (r: r.type == type) (module.resources or []) ++ lib.flatten (map (resourcesInModule type) (module.child_modules or []));
-  resourcesByType = type: resourcesInModule type payload.values.root_module;
+  resourcesByType = type: resourcesInModule type (payload.values.root_module or []);
 in rec {
   resources = resourcesByType "lxd_container";
   resourcesByRole = role: (builtins.filter (r: lib.strings.hasPrefix role r.values.name) resources);
