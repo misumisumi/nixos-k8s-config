93521594ba87f99b930dfcb6c92685cf9aa9def9
Sumi-Sumi <dragon511southern@gmail.com>
2023-06-01 17:52:33 +0900

    fix(src/consts.nix): show.jsonにlabelが存在しないときにもflake環境が立ちあがるようにする

--- src/consts.nix
+++ src/consts.nix
@@ -1,7 +1,13 @@
 {pkgs, ...}: let
   inherit (pkgs.callPackage ./resources.nix {}) resourcesByType;
-  env = (builtins.head (resourcesByType "null_resource")).values.triggers.env;
+  labels = resourcesByType "null_resource";
+  env = (
+    if (builtins.length labels) >= 1
+    then (builtins.head labels).values.triggers.env
+    else "develop"
+  );
 in {
+  ts = resourcesByType "null_resource";
   virtualIP =
     if (env == "product")
     then "192.168.1.50"
