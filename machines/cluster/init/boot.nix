173b9ee415dc72612c2a842bf7cc15cee4c37802
Sumi-Sumi <dragon511southern@gmail.com>
2023-06-07 07:29:00 +0900

    feat: use lxd-nixos for making container and vm image for lxc

--- machines/cluster/init/boot.nix
+++
@@ -1,8 +0,0 @@
-{modulesPath, ...}: {
-  imports = [
-    "${toString modulesPath}/virtualisation/lxc-container.nix"
-  ];
-  security.polkit.enable = true;
-  boot.loader.grub.device = "nodev";
-  time.timeZone = "Asia/Tokyo"; # Time zone and internationalisation
-}
