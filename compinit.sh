2e8351611c1fe786738aeaa40d5b49829a960022
Sumi-Sumi <dragon511southern@gmail.com>
2023-06-26 08:39:05 +0900

    update: add helm compinit

--- compinit.sh
+++ compinit.sh
@@ -1,7 +1,9 @@
 #! /usr/bin/env zsh
-
+KUBECONFIG=.kube/admin.kubeconfig
 [[ $SHELL = *zsh* ]] && source <(kubectl completion zsh)
-alias "k=kubectl --kubeconfig certs/generated/kubernetes/admin.kubeconfig"
+[[ $SHELL = *zsh* ]] && source <(helm completion zsh)
+alias "k=kubectl --kubeconfig $KUBECONFIG"
+alias "he=helm --kubeconfig $KUBECONFIG"
 complete -o default -F __start_kubectl k
 complete -o nospace -C $(which terraform) terraform
 echo "completion loaded"
