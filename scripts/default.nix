9207bb92fd801112e0f4aa036ef89b7911cf11a3
misumisumi <dragon511southern@gmail.com>
2024-01-03 23:57:06 +0900

    fix: making image for lxc

--- scripts/default.nix
+++ scripts/default.nix
@@ -25,7 +25,7 @@
 
   '';
   mkimg4lxc = writeShellScriptBin "mkimg4lxc" ''
-    lxc image import ''$(nixos-generate -f lxc-metadata) ''$(nixos-generate -f lxc --flake ".#lxc-virtual-machine") --alias nixos/lxc-container
+    lxc image import ''$(nixos-generate -f lxc-metadata) ''$(nixos-generate -f lxc --flake ".#lxc-container") --alias nixos/lxc-container
     lxc image import ''$(nixos-generate -f lxc-metadata) ''$(nixos-generate -f qcow --flake ".#lxc-virtual-machine") --alias nixos/lxc-virtual-machine
     lxc image copy images:almalinux/9 local: --auto-update --alias almalinux9/lxc-container
     lxc image copy images:almalinux/9 local: --auto-update --alias almalinux9/lxc-virtual-machine --vm
