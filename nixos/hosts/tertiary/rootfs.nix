c17edaa57641d8c9c37f01b00e9cbfcb78e0ca88
misumisumi <dragon511southern@gmail.com>
2024-01-03 23:56:41 +0900

    update: rootfs config of third node

--- nixos/hosts/tertiary/rootfs.nix
+++ nixos/hosts/tertiary/rootfs.nix
@@ -1,10 +1,11 @@
-{ lib
-, initial ? false
+{ config
+, lib
+, isVM
 , ...
 }:
 let
-  root_device = "/dev/disk/by-id/ata-KIOXIA-EXCERIA_SATA_SSD_822B70LKKLE4";
-  root_device_size = 223.6; # GB
+  root_device = "/dev/disk/by-id/ata-TOSHIBA_THNSNJ256GCSU_84MS106NT7SW";
+  root_device_size = if isVM then 8 else 238.5; # GB
   reserved_size = root_device_size - (root_device_size * 0.85);
 in
 {
@@ -39,12 +40,13 @@ in
     zpool = {
       PoolRootFS = {
         type = "zpool";
-        mountpoint = "/";
         rootFsOptions = {
           compression = "zstd";
           "com.sun:auto-snapshot" = "false";
+          mountpoint = "none";
+          canmount = "off";
         }
-        // lib.optionalAttrs (! initial) {
+        // lib.optionalAttrs (! isVM) {
           encryption = "aes-256-gcm";
           keyformat = "passphrase";
           keylocation = "file:///tmp/rootfs.key";
@@ -59,22 +61,60 @@ in
               reservation = "${builtins.toString reserved_size}G";
             };
           };
-          home = {
+          user = {
+            type = "zfs_fs";
+            options = {
+              mountpoint = "none";
+              canmount = "off";
+              "com.sun:auto-snapshot" = "true";
+            };
+          };
+          "user/home" = {
             type = "zfs_fs";
             mountpoint = "/home";
-            options."com.sun:auto-snapshot" = "true";
           };
-          lxd = {
+          system = {
+            type = "zfs_fs";
+            options = {
+              mountpoint = "none";
+              canmount = "off";
+              "com.sun:auto-snapshot" = "false";
+            };
+          };
+          "system/root" = {
+            type = "zfs_fs";
+            mountpoint = "/";
+            options."com.sun:auto-snapshot" = "false";
+          };
+          "system/var" = {
+            type = "zfs_fs";
+            mountpoint = "/var";
+            options."com.sun:auto-snapshot" = "false";
+          };
+          "system/var/lib" = {
+            type = "zfs_fs";
+            mountpoint = "/var/lib";
+            options."com.sun:auto-snapshot" = "false";
+          };
+          "system/var/lib/lxd" = {
             type = "zfs_fs";
             mountpoint = "/var/lib/lxd";
             options."com.sun:auto-snapshot" = "true";
           };
-          nix = {
+          "local" = {
+            type = "zfs_fs";
+            options = {
+              mountpoint = "none";
+              canmount = "off";
+              "com.sun:auto-snapshot" = "false";
+            };
+          };
+          "local/nix" = {
             type = "zfs_fs";
             mountpoint = "/nix";
           };
         };
-      } // lib.optionalAttrs (! initial) {
+      } // lib.optionalAttrs (! isVM) {
         # use this to read the key during boot
         postCreateHook = ''
           zfs set keylocation="prompt" "PoolRootFS";
