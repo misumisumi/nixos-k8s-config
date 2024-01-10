c7fa5cd6b0286aed531eea102ec174dbf6858fb1
misumisumi <dragon511southern@gmail.com>
2024-01-10 11:52:06 +0900

    fix: "nohibernate" is kernelParams

--- nixos/hosts/init/zfs.nix
+++ nixos/hosts/init/zfs.nix
@@ -1,22 +1,22 @@
 { lib, config, pkgs, ... }:
 {
-  system.activationScripts.keygen-for-initrdssh.text = ''
-    if [ ! -d /etc/secrets/initrd ]; then
-      mkdir -p /etc/secrets/initrd
-      ${pkgs.openssh}/bin/ssh-keygen -t rsa -N "" -f /etc/secrets/initrd/ssh_host_rsa_key
-      ${pkgs.openssh}/bin/ssh-keygen -t ed25519 -N "" -f /etc/secrets/initrd/ssh_host_ed25519_key
-    fi
-  '';
+  # system.activationScripts.keygen-for-initrdssh.text = ''
+  #   if [ ! -d /etc/secrets/initrd ]; then
+  #     mkdir -p /etc/secrets/initrd
+  #     ${pkgs.openssh}/bin/ssh-keygen -t rsa -N "" -f /etc/secrets/initrd/ssh_host_rsa_key
+  #     ${pkgs.openssh}/bin/ssh-keygen -t ed25519 -N "" -f /etc/secrets/initrd/ssh_host_ed25519_key
+  #   fi
+  # '';
   boot = {
     initrd.supportedFilesystems = [ "zfs" ];
     kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
-    kernelModules = [ "nohibernate" ];
+    kernelParams = [ "nohibernate" ];
   };
   services.zfs = {
     trim.enable = true;
     autoScrub.enable = true;
     autoSnapshot = {
-      enable = false;
+      enable = true;
       daily = 7;
       flags = "-k -p --utc";
       frequent = 15;
