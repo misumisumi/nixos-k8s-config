da924707f8b8cf5c3226f689c190ad29f10cb04e
misumisumi <dragon511southern@gmail.com>
2024-01-16 17:28:22 +0900

    fix: terraform-provider-incus

--- patches/default.nix
+++ patches/default.nix
@@ -37,19 +37,9 @@
 # Patch from https://github.com/NixOS/nixpkgs/pull/211600
 { nixpkgs-unstable, ... }:
 final: prev: {
-  inherit (nixpkgs-unstable) doq udev-gothic-nf nixos-anywhere;
+  inherit (nixpkgs-unstable) doq udev-gothic-nf;
   setup-netboot-compornents = prev.callPackage ./setup-netboot-compornents.nix { };
   terraform-providers = prev.terraform-providers // {
-    incus =
-      let
-        hash = "sha256-FWQaU2C1cRo+3SqnesJl5EXfEYR/ssSHHZ9/09zRSTQ=";
-        homepage = "https://registry.terraform.io/providers/lxc/incus";
-        owner = "lxc";
-        repo = "terraform-provider-incus";
-        rev = "v0.0.2";
-        spdx = "MPL-2.0";
-        vendorHash = "sha256-KdyhF1RUZoycsNqRnkME9Q7bTkV2xuNERx24+/p+j1w=";
-      in
-      prev.terraform-providers.lxd.override { inherit hash homepage owner repo rev spdx vendorHash; };
+    inherit (nixpkgs-unstable.terraform-providers) incus;
   };
 }
