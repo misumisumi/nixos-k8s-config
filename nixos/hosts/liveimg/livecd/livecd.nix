86ef75babcd9b0294ffcbbaa4d983304e6372567
misumisumi <dragon511southern@gmail.com>
2024-03-14 11:28:23 +0900

    feat: create livecd include public key of ssh and passwd already set

---
+++ nixos/hosts/liveimg/livecd/livecd.nix
@@ -0,0 +1,10 @@
+{ user, lib, modulesPath, ... }:
+{
+  imports = [
+    (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix")
+    (modulesPath + "/installer/cd-dvd/channel.nix")
+  ];
+  isoImage.squashfsCompression = "zstd -Xcompression-level 6";
+  users.users.root.password = "nixos";
+  users.users.${user}.password = "nixos";
+}
