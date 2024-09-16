725d2f17df15bc0a04f9db98b238429c54f2029b
misumisumi <dragon511southern@gmail.com>
2024-09-16 23:56:34 +0900

    fix(terraform/deploy): follow upstream change

--- terraform/deploy/variables.tf
+++ terraform/deploy/variables.tf
@@ -7,3 +7,20 @@ variable "nodes" {
   )
   description = "target nodes"
 }
+
+variable "ignore_systemd_errors" {
+  type        = bool
+  description = "Ignore systemd errors happening during deploy"
+  default     = false
+}
+
+variable "switch_cmd" {
+  type        = string
+  description = "One required argument, which specifies the desired operation."
+  default     = "switch"
+  validation {
+    condition     = contains(["switch", "boot", "test"], var.switch_cmd)
+    error_message = "The argument must be one of 'switch', 'boot', or 'test'."
+  }
+}
+
