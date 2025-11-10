2088f05cb542b084bd3863d29f81b4fb2ad97d68
misumisumi <s.kobayashi@misumi-sumi.com>
2025-11-10 13:22:08 +0900

    feat: add ssh keys for tiny-router

--- nix/my-lab/baremetal/compute/first-stage/ssh.nix
+++ nix/my-lab/baremetal/compute/first-stage/ssh.nix
@@ -7,6 +7,7 @@
 }:
 let
   homeDir = config.users.users."nixos".home;
+  inherit (lib) mkForce;
 in
 {
   environment.pathsToLink = [ "/share/bash-completion" ];
@@ -22,9 +23,18 @@ in
     extraGroups = [
       "wheel"
     ];
-    initialHashedPassword = lib.mkForce "$y$j9T$2i4ZUQSB0zKtYfaf9YLuZ0$rwxfC/yKFR.zejBm.X00K/JZJSoYVOnnLkSLQ50N5T7";
+    initialHashedPassword = mkForce "$y$j9T$2i4ZUQSB0zKtYfaf9YLuZ0$rwxfC/yKFR.zejBm.X00K/JZJSoYVOnnLkSLQ50N5T7";
+    openssh.authorizedKeys.keys =
+      let
+        inherit (builtins) getEnv readFile;
+        keyFiles = [
+          "${getEnv "HOME"}/.ssh/id_ed25519.pub"
+          ../../../instances/.secrets/tiny-router/ssh/id_ed25519.pub
+        ];
+      in
+      map readFile keyFiles;
   };
-  users.users.root.initialHashedPassword = lib.mkForce "$y$j9T$2i4ZUQSB0zKtYfaf9YLuZ0$rwxfC/yKFR.zejBm.X00K/JZJSoYVOnnLkSLQ50N5T7";
+  users.users.root.initialHashedPassword = mkForce "$y$j9T$2i4ZUQSB0zKtYfaf9YLuZ0$rwxfC/yKFR.zejBm.X00K/JZJSoYVOnnLkSLQ50N5T7";
 
   services.openssh = {
     enable = true;
@@ -51,24 +61,24 @@ in
     fi
     chown ${user}:users -R ${homeDir}/.ssh
   '';
-  systemd.services.pull-host-private-key = {
-    description = "Pull host private SSH key from Vault";
-    after = [
-      "network.target"
-      "network-online.target"
-    ];
-    wants = [ "network-online.target" ];
-    wantedBy = [ "multi-user.target" ];
-    startLimitIntervalSec = 600;
-    startLimitBurst = 5;
-    serviceConfig = {
-      Type = "oneshot";
-      # pull private key using wget from remote ftp host named tiny-router
-      ExecStart = "/bin/sh -c '${pkgs.curl}/bin/curl tftp://tiny-router.kexec/id_ed25519.pub >> ${homeDir}/.ssh/authorized_keys'";
-      RemainAfterExit = "yes";
-      RestartSec = 5;
-      Restart = "on-failure";
-      User = "${user}";
-    };
-  };
+  # systemd.services.pull-host-private-key = {
+  #   description = "Pull host private SSH key from Vault";
+  #   after = [
+  #     "network.target"
+  #     "network-online.target"
+  #   ];
+  #   wants = [ "network-online.target" ];
+  #   wantedBy = [ "multi-user.target" ];
+  #   startLimitIntervalSec = 600;
+  #   startLimitBurst = 5;
+  #   serviceConfig = {
+  #     Type = "oneshot";
+  #     # pull private key using wget from remote ftp host named tiny-router
+  #     ExecStart = "/bin/sh -c '${pkgs.curl}/bin/curl tftp://tiny-router.kexec/id_ed25519.pub >> ${homeDir}/.ssh/authorized_keys'";
+  #     RemainAfterExit = "yes";
+  #     RestartSec = 5;
+  #     Restart = "on-failure";
+  #     User = "${user}";
+  #   };
+  # };
 }
