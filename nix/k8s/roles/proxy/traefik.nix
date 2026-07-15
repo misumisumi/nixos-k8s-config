2369092233b2e4279e0341707c363f5fb7643f33
misumisumi <s.kobayashi@misumi-sumi.com>
2026-07-15 10:55:16 +0900

    feat: add proxy-traefik config

---
+++ nix/k8s/roles/proxy/traefik.nix
@@ -0,0 +1,60 @@
+{
+  lib,
+  static,
+  ...
+}:
+let
+  inherit (lib)
+    mkForce
+    ;
+in
+{
+  services.traefik = {
+    enable = true;
+    staticConfigOptions = {
+      global = {
+        checkNewVersion = false;
+        sendAnonymousUsage = false;
+      };
+      entryPoints = {
+        web = {
+          address = ":80";
+          http.redirections.entryPoint = {
+            to = "websecure";
+            scheme = "https";
+          };
+        };
+        websecure.address = ":443";
+      };
+    };
+    dynamicConfigOptions = {
+      tcp = {
+        routers.k8s-ingress = {
+          rule = "HostSNI(`*`)";
+          entryPoints = [ "websecure" ];
+          service = "k8s-ingress";
+          tls.passthrough = true;
+        };
+        services.k8s-ingress = {
+          loadBalancer.servers = [
+            {
+              address = "ingress.home.:443";
+            }
+          ];
+        };
+      };
+    };
+  };
+
+  networking = {
+    nameservers = [
+      "172.16.1.1"
+    ];
+    firewall = {
+      allowedTCPPorts = [
+        80
+        443
+      ];
+    };
+  };
+}
