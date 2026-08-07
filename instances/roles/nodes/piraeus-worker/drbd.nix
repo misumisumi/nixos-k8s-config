6f872fc49c837f5a226efc770b0c02b92f0c4315
misumisumi <s.kobayashi@misumi-sumi.com>
2026-08-07 13:36:36 +0900

    feat: ensure /usr/src exists for piraeus drbd-module-loader

--- instances/roles/nodes/piraeus-worker/drbd.nix
+++ instances/roles/nodes/piraeus-worker/drbd.nix
@@ -10,4 +10,10 @@
       "drbd"
     ];
   };
+
+  # drbd-module-loader が hostPath で /usr/src をマウントするため、
+  # ディレクトリを常に存在させる。
+  systemd.tmpfiles.rules = [
+    "d /usr/src 0755 root root -"
+  ];
 }
