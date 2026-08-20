22623f2d5dde1d39a57fb9180405114ecdb2c707
misumisumi <s.kobayashi@misumi-sumi.com>
2026-08-20 16:15:19 +0900

    fix(fake-isp): replace with linux-container

--- baremetals/roles/mngr/fake-isp/default.nix
+++ baremetals/roles/mngr/fake-isp/default.nix
@@ -1,4 +1,13 @@
 {
+  inputs,
+  modulesPath,
+  lib,
+  ...
+}:
+let
+  inherit (lib) mkForce;
+in
+{
   imports = [
     ../../share/apps/debug.nix
     ../../share/settings/console.nix
@@ -6,4 +15,8 @@
     ../../share/settings/system.nix
     ./network.nix
   ];
+  image.modules = mkForce {
+    lxc = inputs.homelab-modules.nixosModules.lxc-container;
+    lxc-metadata = modulesPath + "/virtualisation/lxc-image-metadata.nix";
+  };
 }
